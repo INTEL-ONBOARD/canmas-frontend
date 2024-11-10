6f1807df8710a1eaf3d91dedc4076b81bf29c889
B1N4L <163591915+B1N4L@users.noreply.github.com>
2024-11-10 19:10:38 +0530

    dashboard: sidebar

--- canmas-app/src/pages/Dashboard.jsx
+++ canmas-app/src/pages/Dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link, Routes, Route, Outlet } from 'react-router-dom';
 import { useState } from 'react';
 
+import Header from '../components/Header';
+
 function Dashboard() {
 
   const [activeTab, setActiveTab] = useState("home"); // State for current active tab
@@ -9,153 +11,87 @@ function Dashboard() {
 
   return (
     <>
-    <div className="container ml-10 my-32 md:flex">
-      <ul className="flex-column space-y-4 text-sm font-medium text-gray-500 md:me-4 mb-4 md:mb-0">
-        <li>
+    {/*header */}
+    <Header/>
+
+    <div className="container flex ml-10 my-32">
+
+      {/*sidebar */}
+      <div className="flex flex-col justify-center bg-gradient-to-tl from-lime-700 to-green-200 gap-6 text-gray-700 rounded-full p-4">
+
           <Link to="/dashboard/home" 
-            className={`${activeTab === "home" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
-            onClick={() => setActiveTab("home")} // Add click handler here
+            onClick={() => setActiveTab("home")} 
+            className={`${activeTab === "home" ? "inline-flex justify-center text-black text-right  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
           >
-            {/*
-            <svg 
-              //className={`${activeTab === "profile" ? "" : "w-4 h-4 me-2 text-white"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"
-                className={`${activeTab === "home" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-            </svg>
-            */}
-            Home
+            <i className="px-2 fi fi-rs-home  text-[24px]"></i>
           </Link>
-        </li>
-        <li>
+
           <Link to="/dashboard/products" 
-            className={`${activeTab === "products" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("products")}
+            className={`${activeTab === "products" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
           >
-            {/*
-            <svg
-              className={`${activeTab === "products" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18"
-            >
-              <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-            </svg>
-             */}
-            Products
+            <i className="px-2 fi fi-rs-apple-whole  text-[24px]"></i>
           </Link>
-        </li>
-        <li>
+
           <Link to="/dashboard/clients" 
-            className={`${activeTab === "clients" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("clients")}
-          >{/*
-            <svg 
-              className={`${activeTab === "clients" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M18 7.5h-.423l-.452-1.09.3-.3a1.5 1.5 0 0 0 0-2.121L16.01 2.575a1.5 1.5 0 0 0-2.121 0l-.3.3-1.089-.452V2A1.5 1.5 0 0 0 11 .5H9A1.5 1.5 0 0 0 7.5 2v.423l-1.09.452-.3-.3a1.5 1.5 0 0 0-2.121 0L2.576 3.99a1.5 1.5 0 0 0 0 2.121l.3.3L2.423 7.5H2A1.5 1.5 0 0 0 .5 9v2A1.5 1.5 0 0 0 2 12.5h.423l.452 1.09-.3.3a1.5 1.5 0 0 0 0 2.121l1.415 1.413a1.5 1.5 0 0 0 2.121 0l.3-.3 1.09.452V18A1.5 1.5 0 0 0 9 19.5h2a1.5 1.5 0 0 0 1.5-1.5v-.423l1.09-.452.3.3a1.5 1.5 0 0 0 2.121 0l1.415-1.414a1.5 1.5 0 0 0 0-2.121l-.3-.3.452-1.09H18a1.5 1.5 0 0 0 1.5-1.5V9A1.5 1.5 0 0 0 18 7.5Zm-8 6a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7Z"/>
-            </svg>
-            */}
-            Clients
+            className={`${activeTab === "clients" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
+          >
+            <i className="px-2 fi fi-rs-user  text-[24px]"></i>
           </Link>
-        </li>
-        <li>
+
           <Link to="/dashboard/orders" 
-            className={`${activeTab === "orders" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("orders")}
-          >{/*
-            <svg 
-              className={`${activeTab === "orders" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M7.824 5.937a1 1 0 0 0 .726-.312 2.042 2.042 0 0 1 2.835-.065 1 1 0 0 0 1.388-1.441 3.994 3.994 0 0 0-5.674.13 1 1 0 0 0 .725 1.688Z"/>
-              <path d="M17 7A7 7 0 1 0 3 7a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h1a1 1 0 0 0 1-1V7a5 5 0 1 1 10 0v7.083A2.92 2.92 0 0 1 12.083 17H12a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v1a2 2 0 0 0 2 2h3a4.92 4.92 0 0 0 4.917-4.917V10a3 3 0 0 0-3-3Z"/>
-            </svg>
-            */}
-            Orders
+            className={`${activeTab === "orders" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
+          >
+            <i className="px-2 fi fi-rs-shopping-cart  text-[24px]"></i>
           </Link>
-        </li>
-        <li>
+
           <Link to="/dashboard/suppliers" 
-            className={`${activeTab === "suppliers" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("suppliers")}
-          >{/*
-            <svg 
-              className={`${activeTab === "suppliers" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M7 7h10M7 12h10M7 17h10"/>
-            </svg>
-            */}
-            Suppliers
+            className={`${activeTab === "suppliers" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
+          >
+            <i className="px-2 fi fi-rs-supplier-alt  text-[24px]"></i>
           </Link>
-        </li>
-        <li>
+
           <Link to="/dashboard/sales" 
-            className={`${activeTab === "sales" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("sales")}
+            className={`${activeTab === "sales" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
           >
-            {/*
-            <svg 
-              className={`${activeTab === "sales" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M5 12h14M12 5v14"/>
-            </svg>
-             */}
-            Sales
+            <i className="px-2 fi fi-rs-benefit-porcent  text-[24px]"></i>
           </Link>
-        </li>
-        <li>
+
           <Link to="/dashboard/reports" 
-            className={`${activeTab === "reports" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("reports")}
+            className={`${activeTab === "reports" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
           >
-            {/*
-            <svg 
-              className={`${activeTab === "reports" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M12 2l8 8-8 8-8-8 8-8z"/>
-            </svg>
-             */}
-            Reports
+            <i className="px-2 fi fi-rs-file-medical-alt  text-[24px]"></i>
           </Link>
-        </li>
-        <li>
+
           <Link to="/dashboard/faqs" 
-            className={`${activeTab === "faqs" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("faqs")}
+            className={`${activeTab === "faqs" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
           >
-            {/*
-            <svg 
-              className={`${activeTab === "faqs" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M12 2l8 8-8 8-8-8 8-8z"/>
-            </svg>
-             */}
-            Faqs
+            <i className="px-2 fi fi-rs-comment-question  text-[24px]"></i>
           </Link>
-        </li>
-        <li>
+
           <Link to="/dashboard/settings" 
-            className={`${activeTab === "settings" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
             onClick={() => setActiveTab("settings")}
+            className={`${activeTab === "settings" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
           >
-            {/*
-            <svg 
-              className={`${activeTab === "settings" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M12 2l8 8-8 8-8-8 8-8z"/>
-            </svg>
-             */}
-            Settings
+            <i className="px-2 fi fi-rs-settings  text-[24px]"></i>
           </Link>
-        </li>
-        {/*
-        <li>
-          <Link to="/dashboard/settings" 
-            className={`${activeTab === "logout" ? "inline-flex items-center px-4 py-3 text-white bg-green-600 rounded-lg active w-full" : "inline-flex items-center px-4 py-3 rounded-lg hover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full"}`}
+
+          <div className='mt-5 mb-5'><hr /></div>
+
+          <Link to="#" 
             onClick={() => setActiveTab("logout")}
+            className={`${activeTab === "logout" ? "inline-flex justify-center text-black  rounded-full bg-gray-100" : "inline-flex justify-center rounded-full hover:text-gray-700 hover:bg-gray-100"}`}
           >
-          */}
-            {/*
-            <svg 
-              className={`${activeTab === "settings" ? "w-4 h-4 me-2 text-white" : "w-4 h-4 me-2 text-gray-500"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path d="M12 2l8 8-8 8-8-8 8-8z"/>
-            </svg>
-             */}
-             {/*
-            Logout
+            <i className="px-2 fi fi-rs-arrow-left-from-arc rotate-180  text-[24px]"></i>
           </Link>
-        </li>
-             */}
-      </ul>
+
+      </div>
 
       {/*outlet for tab section in the dashboard*/}
       <div className="">
